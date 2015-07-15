1f4602c3194d0d14cbb9febefaa187d092221680
Caleb Williams <caleb.w27@me.com>
2015-07-14 20:47:33 -0700

    create music wall

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,17 @@
 get '/' do
   erb :index
 end
+
+get '/songs' do
+  @songs = Song.all
+  erb :songs
+end
+
+post '/songs' do
+  Song.create(params)
+  redirect '/'
+end
+
+not_found do
+  "Not found! Go Away"
+end
